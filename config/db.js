c194625db6057efda79c41fd27f65b9b5ce9c416
Arun9123n <145862247+Arun9123n@users.noreply.github.com>
2024-01-24 21:08:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require ("express");
 
-// const connectDB = "mongodb+srv://User2000:test9123@cluster0.3m1jaqe.mongodb.net//myFirstDatabase?retryWrites=true&w=majority";
+ const connectDB = "mongodb+srv://User2000:test9123@cluster0.3m1jaqe.mongodb.net//myFirstDatabase?retryWrites=true&w=majority";
 
-// mongoose.connect(connectDB) .then(() => {
-//     console.log("connect succesfully MDB")
-// })
-// .catch((error) => {
-//     console.log(error)
-// });
+ mongoose.connect(connectDB) .then(() => {
+     console.log("connect succesfully MDB")
+ })
+ .catch((error) => {
+     console.log(error)
+ });
 
 
 
