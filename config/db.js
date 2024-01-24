ef8a53ed49fea6886671b5cdafae1bc9730a724d
Arun9123n <145862247+Arun9123n@users.noreply.github.com>
2024-01-24 21:07:42 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require ("express");
+
+// const connectDB = "mongodb+srv://User2000:test9123@cluster0.3m1jaqe.mongodb.net//myFirstDatabase?retryWrites=true&w=majority";
+
+// mongoose.connect(connectDB) .then(() => {
+//     console.log("connect succesfully MDB")
+// })
+// .catch((error) => {
+//     console.log(error)
+// });
+
+
+
